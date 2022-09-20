6cda357a1b6111621ddb1dd70bef08feac9c0113
Faisal Feroz <muhammad.faisal@gmail.com>
2022-09-21 01:09:44 +0500

    Initial Commit

---
+++ BankingApplication/BankingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApplicationApp.swift
+//  BankingApplication
+//
+//  Created by Faisal Feroz on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BankingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
